319202dcbae3ed693e77c1e1c0dfbc5bba8b4cbb
Deepika Juneja <deepsjunejadj@gmail.com>
2021-05-19 03:55:30 +0530

    Added code to enable registered users to reset password

--- app/src/main/java/com/example/stopnjot/LoginActivity.java
+++ app/src/main/java/com/example/stopnjot/LoginActivity.java
@@ -11,6 +11,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -24,7 +25,8 @@ public class LoginActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private EditText emailTextView, passwordTextView;
     private Button loginButton;
-    private TextView signUpTextView;
+    private TextView signUpTextView, fgtPwdBtnView;
+    private String regEmail;
 //    private ProgressBar progressbar;
 
     @Override
@@ -37,6 +39,7 @@ public class LoginActivity extends AppCompatActivity {
         passwordTextView = findViewById(R.id.editTextTextPassword);
         loginButton = findViewById(R.id.loginButton);
         signUpTextView = findViewById(R.id.signUpClickableText);
+        fgtPwdBtnView = findViewById(R.id.forgotPasswordBtn);
 //        progressbar = findViewById(R.id.progressBar);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
@@ -51,6 +54,13 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent signupIntent = new Intent(LoginActivity.this, SignupActivity.class);
                 startActivity(signupIntent);
+            }
+        });
+
+        fgtPwdBtnView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showResetPasswordDialogBox();
             }
         });
     }
@@ -110,5 +120,48 @@ public class LoginActivity extends AppCompatActivity {
 
     private void updateUI(FirebaseUser user) {
 
+    }
+
+    public void showResetPasswordDialogBox() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        final View boxView = getLayoutInflater().inflate(R.layout.fgtpwd_dialogbox_layout, null);
+
+        EditText emailID = (EditText)boxView.findViewById(R.id.editTextRegEmail);
+        Button okButton = (Button)boxView.findViewById(R.id.confirmPasswordReset);
+        Button cancelButton = (Button)boxView.findViewById(R.id.cancelPasswordReset);
+
+        builder.setView(boxView);
+        AlertDialog alertDialog = builder.create();
+        alertDialog.setCanceledOnTouchOutside(false);
+
+        okButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                regEmail = emailID.getText().toString();
+                resetPassword();
+                alertDialog.dismiss();
+            }
+        });
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alertDialog.dismiss();
+            }
+        });
+        alertDialog.show();
+    }
+
+    private void resetPassword() {
+        mAuth.sendPasswordResetEmail(regEmail).addOnCompleteListener(
+                new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()) {
+                    Log.i(TAG, "Password Reset Email sent.");
+                    Toast.makeText(LoginActivity.this, "Password Reset Email sent.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
